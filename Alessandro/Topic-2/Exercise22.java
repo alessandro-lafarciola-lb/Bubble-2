2a2ad8c8f558a6dc974adc0b46711b68acc04419
AlessandroLaFarciola <104493870+AlessandroLaFarciola@users.noreply.github.com>
2022-04-28 17:27:49 +0100

    Added Exercise25, Updated Exercise22

--- Alessandro/Topic-2/Exercise22.java
+++ Alessandro/Topic-2/Exercise22.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercise22 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Type your age: ");
+        System.out.print("Type your age: ");
         int age = s.nextInt();
 
         double price = 7;
