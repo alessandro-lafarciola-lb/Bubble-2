7aef19fff08fc37b8ad638f74d9cec332a09d5c4
AlessandroLaFarciola <104493870+AlessandroLaFarciola@users.noreply.github.com>
2022-04-28 15:00:36 +0100

    Updated Exercise22

--- Alessandro/Topic-2/Exercise23.java
+++ Alessandro/Topic-2/Exercise23.java
@@ -1,1 +1,13 @@
-// X
+class Exercise23 {
+    public static void main(String[] args) {
+        String text_a = "Hello";
+        String text_b = "hello";
+
+        if(text_a.equals(text_b))
+            System.out.println("1 - Both texts are equal!");
+        if(text_a.equalsIgnoreCase(text_b))
+            System.out.println("2 - Both texts are equal!");
+        if(text_a == text_b)
+            System.out.println("3 - Both texts are equal!");
+    }
+}
