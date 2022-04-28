ad39177ca17f9906b78f084e0787bde84bafcfad
simaoantunes15 <simaoantunes1@hotmail.com>
2022-04-28 14:33:05 +0100

    first commit

---
+++ Simao/Topic_1/Exercice_1_1.java
@@ -0,0 +1,29 @@
+package JavaTopic_1;
+
+import java.util.Scanner;
+
+public class Exercice_1_1 {
+
+    public static void main(String[] args){
+
+        //Get the Season of the year
+        System.out.println("Insert any Season of the year.");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+
+        //Get the number
+        System.out.println("Insert any number.");
+        int number = scanner.nextInt();
+
+        //Get an adjective
+        System.out.println("Insert any adjective.");
+        String adjective = scanner.next();
+        scanner.close();
+
+        //Insert the information in the sentence
+        System.out.println("On a " + adjective + " " + season
+                + " day, I drink a minimum of " + number + " cups of coffee.");
+
+    }
+
+}
---
+++ Simao/Topic_1/Exercice_1_3.java
@@ -0,0 +1,22 @@
+package JavaTopic_1;
+
+public class Exercice_1_3 {
+    public static void main(String[]args){
+        //+
+        int x = 8;
+        int y = 2;
+        int result1 = x + y;
+        System.out.println("The result of x + y = " + result1);
+
+        //-
+        int result2 = x - y;
+        System.out.println("The result of x - y = " + result2);
+        //*
+        int result3 = x * y;
+        System.out.println("The result of x * y = " + result3);
+
+        ///
+        double result4 = x / y;
+        System.out.println("The result of x / y = " + result4);
+    }
+}
