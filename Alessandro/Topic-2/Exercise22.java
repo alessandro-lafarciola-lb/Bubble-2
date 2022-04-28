5ffb978e8bbdc20fd5dd950f2488b7ec2eddbf11
AlessandroLaFarciola <104493870+AlessandroLaFarciola@users.noreply.github.com>
2022-04-28 14:26:55 +0100

    Added Topic-2, Exercise21/22

---
+++ Alessandro/Topic-2/Exercise22.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Type your age: ");
+        int age = s.nextInt();
+
+        double price = 7;
+
+        if(age < 5)
+            price = price * 0.4; // 60% Discount
+        if(age > 55)
+            price = price * 0.45; // 55% Discount
+
+        System.out.println("The price is €" + price);
+    }
+}
