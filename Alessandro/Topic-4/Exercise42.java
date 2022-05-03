5e5da46ef3cef6c97bd7ca74a1d0a8ac5cdab19d
AlessandroLaFarciola <104493870+AlessandroLaFarciola@users.noreply.github.com>
2022-05-03 12:56:55 +0100

    Updated Exercises41/42/43/44

--- Alessandro/Topic-4/Exercise42.java
+++ Alessandro/Topic-4/Exercise42.java
@@ -1,32 +1,48 @@
 import java.util.Scanner;
 
 public class Exercise42 {
+    public static boolean isNumber(String text) {
+        boolean number = true;
+        if(text.length() == 0)
+            number = false;
+        for(int i = 0; i < text.length(); i++)
+            if(!Character.isDigit(text.charAt(i)))
+                number = false;
+        return number;
+    }
+    public static void showUserName(String name) {
+        System.out.println("Hello " + name + "!");
+    }
+    public static void showUserAge(int age) {
+        System.out.println("You are " + age + " years old!");
+    }
+    public static void isUserZombie(String zombie) {
+        if(zombie.equalsIgnoreCase("Y"))
+            System.out.println("You are a zombie!!!");
+        else
+            System.out.println("You are not a zombie.");
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Type your name: ");
-        String name = s.next();
-        System.out.print("Type your age: ");
-        int age = s.nextInt();
-        System.out.println("Are you a zombie! (Y/N)");
-        String zombie = s.next();
+        String name = s.nextLine().trim();
+
+        String text = "";
+        while(!isNumber(text)) {
+            System.out.print("Type your age: ");
+            text = s.nextLine();
+        }
+        int age = Integer.parseInt(text);
+
+        String zombie = "";
+        while(!zombie.equalsIgnoreCase("Y")
+                && !zombie.equalsIgnoreCase("N")) {
+            System.out.println("Are you a zombie? (Y/N)");
+            zombie = s.nextLine();
+        }
 
         showUserName(name);
         showUserAge(age);
         isUserZombie(zombie);
-    }
-
-    public static void showUserName(String name) {
-        System.out.println("Hello " + name + "!");
-    }
-
-    public static void showUserAge(int age) {
-        System.out.println("You are " + age + " years old!");
-    }
-
-    public static void isUserZombie(String zombie) {
-        if(zombie.equals("Y"))
-            System.out.println("You are a zombie!!!");
-        else
-            System.out.println("You are not a zombie.");
     }
 }
--- Alessandro/Topic-4/Exercise44.java
+++ Alessandro/Topic-4/Exercise44.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Exercise44 {
-    public static boolean isNumber(String our_pin) {
+    public static boolean isNumber(String text) {
         boolean number = true;
-        for(int i = 0; i < our_pin.length(); i++)
-            if(!Character.isDigit(our_pin.charAt(i)))
+        if(text.length() == 0)
+            number = false;
+        for(int i = 0; i < text.length(); i++)
+            if(!Character.isDigit(text.charAt(i)))
                 number = false;
         return number;
     }
@@ -16,10 +18,10 @@ public class Exercise44 {
 
         for(int i = 3; i > 0; i--) {
             System.out.print("Type a 4-digit pin: ");
-            our_pin = s.next();
+            our_pin = s.nextLine();
             while(!isNumber(our_pin) || our_pin.length() != 4) {
-                System.out.print("Error. Type a 4-digit pin: ");
-                our_pin = s.next();
+                System.out.print("Error. Type another 4-digit pin: ");
+                our_pin = s.nextLine();
             }
 
             if(our_pin.equals(pin)) {
@@ -28,6 +30,8 @@ public class Exercise44 {
             }
             else if(i == 1)
                 System.out.println("Access denied. Your account was blocked.");
+            else
+                System.out.println("Incorrect pin.");
         }
     }
 }
