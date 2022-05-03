6e0628552e3d39a97b336282d70a86ce3b71df39
AlessandroLaFarciola <104493870+AlessandroLaFarciola@users.noreply.github.com>
2022-05-03 13:09:16 +0100

    Updated Exercise42

--- Alessandro/Topic-4/Exercise42.java
+++ Alessandro/Topic-4/Exercise42.java
@@ -25,7 +25,7 @@ public class Exercise42 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Type your name: ");
-        String name = s.nextLine().trim();
+        String name = s.nextLine().trim().replaceAll(" +", " ");
 
         String text = "";
         while(!isNumber(text)) {
